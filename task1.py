4a4395a46eccf87815b6ba846b54b6e9c806ee1a
qqreyn <duch.jan.8@gmail.com>
2026-02-21 17:53:02 +0100

    Replace 'Cereal' with 'Apple' in food list

--- task1.py
+++ task1.py
@@ -1,7 +1,8 @@
-list = ["Pizza", "Salad", "Burger", "Sandwich", "Cereal"]
+list = ["Pizza", "Salad", "Burger", "Sandwich", "Apple"]
 
 print(list[0])
 print(list[1])
 print(list[2])
 print(list[3])
 print(list[4])
+
